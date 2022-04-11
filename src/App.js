39bb1975a9415de5eb86e41a9cc9e7dbe90913da
Ellen Seghers <ellen.seghers1@gmail.com>
2022-04-11 16:20:13 +0200

    Create a separate file for the form and add it to app.js.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Todos from './Todos';
+import Form from './Form';
 
 const App = () => {
     return (
         <body>
             <Todos />
+            <Form />
         </body>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,11 @@
+const Form = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Write a new todo" />
+            <br />
+            <button>Add todo</button>
+        </div>
+    )
+}
+
+export default Form;
