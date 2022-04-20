1f5f1b214bfef08639c0342c6b3397a0b6d3c317
Ellen Seghers <ellen.seghers1@gmail.com>
2022-04-20 11:40:52 +0200

    Add form and Todos

---
+++ second/src/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Form = () => {
+    return (
+        <div>
+            <input type="text" id="fname"/>
+            <button>Add Todo</button>
+        </div>
+    )
+}
+
+export default Form;
---
+++ second/src/Todos.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Todos = () => {
+    return (
+        <ul>
+            <li>
+                1st todo
+            </li>
+        </ul>
+    )
+}
+
+export default Todos;
