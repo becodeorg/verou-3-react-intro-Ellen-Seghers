a4e7fc5b06bf40a8573c28a6cf1322968243c281
Ellen Seghers <ellen.seghers1@gmail.com>
2022-04-21 14:21:49 +0200

    Adding functionality to store checkbox state

--- second/src/Todolist.js
+++ second/src/Todolist.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 
 const Todolist = () => {
-    const initialTodos = [{"content": "My first todo", "id": 1}, {"content": "My second todo", "id": 2}];
-    const [todos, setTodos] = useState(initialTodos);
+    const initialTodos = [{"isFinished": false, "content": "My first todo", "id": 1}, {"isFinished": false, "content": "My second todo", "id": 2}];
+    const [todos, updateTodos] = useState(initialTodos);
+
+    const toggleCheckbox = (event) => {
+        todos.map((todoItem) => {
+            if (todoItem.id == event.target.id) {
+                todoItem.isFinished = !todoItem.isFinished;
+            }
+            return todoItem;
+        });
+        updateTodos([...todos]);
+    }
+
     return (
         <ul>
             {todos.map((todoItem) => (
                 <li key={todoItem.id}>
-                    <input type="checkbox" /> {todoItem.content}
+                    <input type="checkbox" checked={todoItem.isFinished} onChange={toggleCheckbox} id={todoItem.id}/> {todoItem.content}
                 </li>
             ))}
         </ul>
