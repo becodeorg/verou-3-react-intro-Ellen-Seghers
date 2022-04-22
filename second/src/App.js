06ade9f6d3a7b99b56e6564d5ff3175818a6baf6
Ellen Seghers <ellen.seghers1@gmail.com>
2022-04-22 14:51:32 +0200

    Stored todos in local storage

--- second/src/App.js
+++ second/src/App.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import Form from './Form';
 import Todolist from "./Todolist";
 
+const LSKEY = "MyTodoApp";
+
 function App() {
     const initialTodos = [];
     const [todos, updateTodos] = useState(initialTodos);
+
+    useEffect(() => {
+        let storedTodos = JSON.parse(window.localStorage.getItem(LSKEY + ".todos"));
+        console.log(storedTodos);
+        if(storedTodos.length > 0) {
+            updateTodos(storedTodos);
+        }
+    }, []);
+
+    useEffect(() => {
+        window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+    }, [todos]);
 
     return (
         <div>
